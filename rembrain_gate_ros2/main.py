a994f96c3d86c48188721457118e5609177b64c3
Vasily Morzhakov <morzhakovva@gmail.com>
2022-05-04 13:19:29 +0000

    parameters added

--- rembrain_gate_ros2/main.py
+++ rembrain_gate_ros2/main.py
@@ -6,6 +6,11 @@ class GateNode(Node):
         super().__init__("rembrain_gate_ros2")
         self.get_logger().info("This node just says 'Hello'")
 
+        self.declare_parameter('in',None)
+        print(self.get_parameter('in').get_parameter_value().string_array_value)
+
+
+
 def main_func(args=None):
     rclpy.init(args=args)
     node = GateNode()
